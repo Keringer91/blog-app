ac2f95de5fb7ea2f26351ef9061d54a24c0628cf
Sabolc Keringer <keringersz@gmail.com>
2019-11-29 20:50:19 +0100

    Zadatak 6: Post form validation added

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h5>Add new post:</h5>
-    <form @submit.prevent>
+    <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul  v-for="(error, index) in errors" :key="index">
+            <li>{{ error }}</li>
+        </ul>
+    </p>
+    <form @submit.prevent >
             <label>Title:</label>
-            <input v-model = "post.title" type="text" id="title"><br>
+            <input v-model = "post.title" type="text" id="title" :minlength="2"><br>
             <label>Text:</label>
-            <input v-model = "post.text" type="text" id="text"><br>
+            <textarea v-model = "post.text" type="text" id="text"></textarea><br>
             <input type="button" value="Reset Form" onClick="this.form.reset()"/><br>
-            <button @click="addNewPost();" type="submit">Submit</button>
+            <button @click="checkForm(); addNewPost();" type="submit" value="Submit">Submit</button>
     </form> 
   </div>
 </template>
@@ -17,15 +23,35 @@ import { postService } from "../services/Posts";
 export default {
   data() {
     return {
-      post: {}
+      post: {},
+      errors: []
     };
   },
   methods: {
+    checkForm() {
+       this.errors = [];
+
+      if (!this.post.title) {
+        this.errors.push("Title required.");
+      }
+      else if (this.post.title.length <2) {
+        this.errors.push("Title should be at least 2 character long.");
+      }
+      if (!this.post.text) {
+        this.errors.push('Content required.');
+      }
+      else if (this.post.text.length > 300) {
+        this.errors.push("Content cant be longer than 300 characters");
+      } 
+    },
     addNewPost() {
+     
+      if (!this.errors.length) {
         postService.addPost(this.post).then(() => {
           this.$router.push("/posts");
           this.post = {};
         });
+      }
     }
   }
 };
