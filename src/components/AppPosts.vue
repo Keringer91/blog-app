b0a3be384090fd790beb43bc2d2551563b551b44
Sabolc Keringer <keringersz@gmail.com>
2019-11-29 18:44:20 +0100

    Zadatak 2: /posts displays posts from response, Posts services added

---
+++ src/components/AppPosts.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+   <h3>All Posts</h3>
+   <ul v-for="(post, index) in posts" :key="index">
+        <li>
+            Post's ID: {{post.id }}<br>
+            Title: {{ post.title }}<br>
+            Text: {{ post.text }}<br>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { postService } from "../services/Posts";
+export default {
+  name: "AppPosts",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  methods: {
+   
+  },
+  created() {
+    postService.getAll().then(response => {
+      this.posts = response.data;
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
